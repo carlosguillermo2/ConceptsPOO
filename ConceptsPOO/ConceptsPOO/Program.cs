aa5fa5c4291777e1d3f564a834eb9c31574ab5a4
carlosguillermo2 <cguillermo@gmail.com>
2022-02-23 08:43:56 +0100

    Finish date Class

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConceptsPOO;
+
+Console.WriteLine("Concepts POO");
+Console.WriteLine("============");
+
+try
+{
+    Console.WriteLine(new Date(2022, 02, 20));
+    Console.WriteLine(new Date(2022, 16, 92));
+}
+catch (Exception error)
+{
+
+    Console.WriteLine(error.Message);
+}
+
+
